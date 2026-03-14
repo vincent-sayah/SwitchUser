40da6cffe34386464e6b6fbb4299be93a58f2169
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-03-14 11:59:25 +0100

    Delete SwitchUser directory

--- SwitchUser/plugin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$id = 'swus';
-$version = '1.0.0';
-$ilias_min_version = '10.0';
-$ilias_max_version = '10.999';
-$responsible = 'Vincent SAYAH';
-$responsible_mail = 'vince.syh@free.fr';
-$supports_cli_setup = true;
